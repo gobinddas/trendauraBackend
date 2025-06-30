25d2e2e6bcf173a72dfdbbef5d339b930e0b55d9
Gobind Das <gobind98077@gmail.com>
2025-06-30 18:22:30 +0545

    initial setup

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+
+import Dashboard from "./pages/Dashboard"
+import ProductManagement from "./pages/ProductManagement"
+import CategoryManagement from "./pages/CategoryManagement"
+import UserManagement from "./pages/UserManagement"
+import OrderManagement from "./pages/OrderManagement"
+import CardWishlist from "./pages/CardWishlist"
+import Setting from "./pages/Setting"
+import Reporting from "./pages/Reporting"
+
+
+function App() {
+
+
+  return (
+    <>
+     hey
+    </>
+  )
+}
+
+export default App
---
+++ src/pages/CardWishlist.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const CardWishlist = () => {
+  return (
+    <div>CardWishlist</div>
+  )
+}
+
+export default CardWishlist
